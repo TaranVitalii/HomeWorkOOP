b63a1dde50fc0e20738fc3d94dcf5900c0afd5ca
ikn0wQQ <kn0wnOutCast@gmail.com>
2019-06-25 13:52:31 +0300

    good version

--- src/js.js
+++ src/js.js
@@ -1,72 +1,61 @@
 'use strict';
-class Burger {
-	constructor(){
-	}	
-	
-	getPrice(){
-		const reducer = (accumulator, currentValue) => accumulator + currentValue;
-		return this._price.reduce(reducer);
-	}
-	
-
-	getCalories(){
-		const reducer = (accumulator, currentValue) => accumulator + currentValue;
-		return this._calories.reduce(reducer);
-	}
-}
-
 // ======================================================гамбургер===============================
-class Hamburger extends Burger{
-	_price = [];
-	_calories = [];
+class Hamburger {
+	_price = 0;
+	_calories = 0;
 	constructor(size){
-		super()
+		this.allowedSauce = ['SAUCE_MAYO','SAUCE_MUSTARD'];
+		this.checkSize(size);
+	}	
+	checkSize(size){
 		try{
-			if(JSON.stringify(size) === JSON.stringify(Hamburger.SIZE_BIG) || JSON.stringify(size) === JSON.stringify(Hamburger.SIZE_SMALL)){
+			if(size === Hamburger.SIZE_BIG || size === Hamburger.SIZE_SMALL){
 				this.size = size;
-				this._price.push(size.price);
-				this._calories.push(size.calories);
+				this._price += size.price;
+				this._calories += size.calories;
 			}else {throw new Error("uncorrect size Hamburger");}
 		}catch(e){
 			console.log(e.message)
 		}
-	}	
-	static get SIZE_SMALL(){
-		const size = {
-				price:30,
-				calories:200,
-			};
-			return size;
-	}
-	static get SIZE_BIG(){
-		const size = {
-			price: 50,
-			calories: 320
-		};
-		return size;
 	}
 
-	static get SAUCE_MAYO(){
-		let sauce = {
-			price: 5,
-			calories:30,
+		static #sizeSmall = {
+			price:30,
+			calories:200,
 		}
-		return sauce;
-	}
 
-	static get SAUCE_MUSTARD(){
-		let sauce = {
+	static get SIZE_SMALL(){
+		return this.#sizeSmall;
+	}
+		static #sizeBig = {
+			price: 50,
+			calories: 320,
+		}
+	static get SIZE_BIG(){
+		return this.#sizeBig;
+	}
+		static #sauceMayo = {
+				name: 'SAUCE_MAYO',
+				price: 5,
+				calories:30,
+			}
+	static get SAUCE_MAYO(){
+		return this.#sauceMayo;
+	}
+		static #sauceMustard = {
+			name: 'SAUCE_MAYO',
 			price: 5,
 			calories:35,
 		}
-		return sauce;
+	static get SAUCE_MUSTARD(){
+		return this.#sauceMustard;
 	}
 
 	addSauce(value){
 		try{
-			if(JSON.stringify(value) === JSON.stringify(Hamburger.SAUCE_MAYO) ||JSON.stringify(value) === JSON.stringify(Hamburger.SAUCE_MUSTARD)){
-				this._price.push(value.price);
-				this._calories.push(value.calories);
+			if(this.allowedSauce.includes(value.name)){
+				this._price += value.price;
+				this._calories += value.calories;
 			}else{throw new Error("uncorrect sauce add to Hamburger");}
 		}catch(e){
 			console.log(e.message)
@@ -74,58 +63,74 @@ class Hamburger extends Burger{
 	}
 	removeSauce(value){
 		try{
-			if(JSON.stringify(value) === JSON.stringify(Hamburger.SAUCE_MAYO) ||JSON.stringify(value) === JSON.stringify(Hamburger.SAUCE_MUSTARD)){
-			this._price.push((-1)* value.price);
-			this._calories.push((-1)*value.calories);
+			if(this.allowedSauce.includes(value.name)){
+			this._price -= value.price;
+			this._calories -= value.calories;
 			}else{throw new Error("uncorrect sauce for remove from Hamburger");}
 		}catch(e){
 			console.log(e.message)
 		}
 	}
+	getPrice(){
+		return this._price;
+	}
+	
+
+	getCalories(){
+		return this._calories;
+	}
 }
 // =======================================================чизбургер======================================
 
-class Cheeseburger extends Burger {
-	_price = [];
-	_calories = [];
+class Cheeseburger extends Hamburger {
 	constructor(size){
-		super()
-			try{
-				if(JSON.stringify(size) === JSON.stringify(Cheeseburger.SIZE_BIG) || JSON.stringify(size) === JSON.stringify(Cheeseburger.SIZE_SMALL)){
-					this.size = size;
-					this._price.push(size.price);
-					this._calories.push(size.calories);
-				} else {throw new Error("uncorrect size Cheeseburger");}
-			} catch(e) {
-				console.log(e.message)
-			}
+		super(size);
+		this.allowedSauce = ['SAUCE_MAYO','SAUCE_MUSTARD','SAUCE_TARTAR'];
 	}
-	static get SIZE_SMALL(){
-	const size ={
+
+	checkSize(size){
+
+		try{
+			if(size === Cheeseburger.SIZE_BIG || size === Cheeseburger.SIZE_SMALL){
+				this.size = size;
+				this._price += size.price;
+				this._calories += size.calories;
+			}else {throw new Error("uncorrect size cheeseburger");}
+		}catch(e){
+			console.log(e.message)
+		}
+	}
+		static #sizeSmall = {
 			price:40,
 			calories:250,
-		};
-		return size;
+		}
+
+	static get SIZE_SMALL(){
+		return this.#sizeSmall
 	}
-	static get SIZE_BIG(){	
-	const size ={
+		static #sizeBig = {
 			price: 70,
 			calories: 400
-		};
-		return size; 
+		}
+
+	static get SIZE_BIG(){	
+		return this.#sizeBig; 
 	}
+	static #sauceTartar = {
+				name: 'SAUCE_TARTAR',
+				price: 7,
+				calories:37
+			}
+
 	static get SAUCE_TARTAR(){
-	const size = {
-			price: 7,
-			calories:37
-		};
-		return size;
+		return this.#sauceTartar;
 	}
+
 	addSauce(value){
 		try{
-			if(JSON.stringify(value) === JSON.stringify(Hamburger.SAUCE_MAYO) || JSON.stringify(value) === JSON.stringify(Hamburger.SAUCE_MUSTARD) || JSON.stringify(value) === JSON.stringify(Cheeseburger.SAUCE_TARTAR)){
-				this._price.push(value.price);
-				this._calories.push(value.calories);
+			if(this.allowedSauce.includes(value.name)){
+				this._price += value.price;
+				this._calories += value.calories;
 			}else{throw new Error("uncorrect sauce for add to Cheeseburger");}
 		}catch(e){
 			console.log(e.message)
@@ -133,9 +138,9 @@ class Cheeseburger extends Burger {
 	}
 	removeSauce(value){
 		try{
-			if(JSON.stringify(value) === JSON.stringify(Hamburger.SAUCE_MAYO) ||JSON.stringify(value) === JSON.stringify(Hamburger.SAUCE_MUSTARD) || JSON.stringify(value) === JSON.stringify(Cheeseburger.SAUCE_TARTAR)){
-			this._price.push((-1)* value.price);
-			this._calories.push((-1)*value.calories);
+			if(this.allowedSauce.includes(value.name)){
+			this._price -= value.price;
+			this._calories -= value.calories;
 			}else{throw new Error("uncorrect sauce for remove from Cheeseburger");}
 		}catch(e){
 			console.log(e.message)
